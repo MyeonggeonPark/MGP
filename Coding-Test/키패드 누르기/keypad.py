7bea9208c4c1d02cb2b9fca16639d308c4394644
MyeonggeonPark <kack7090@gmail.com>
2021-06-15 17:06:09 +0900

    210615 coding test edit

--- Coding-Test/키패드 누르기/keypad.py
+++ Coding-Test/키패드 누르기/keypad.py
@@ -71,12 +71,15 @@ def get_distance(keypad, finger_position, next_number):
 
 def solution_a(numbers, hand):
     result = ''
+    #키패드를 좌표로 정의
     keypad = { 1: [0, 0], 2: [0, 1], 3: [0, 2], 4: [1, 0], 5: [1, 1], 6: [1, 2], 7: [2, 0], 8: [2, 1], 9: [2, 2], 0: [3, 1] }
     
+    #손가락에 따른 숫자 위치 리스트 만들기
     left_finger_numbers = [1, 4, 7]
     right_finger_numbers = [3, 6, 9]
+    #변수가 되는 부분
     center_finger_numbers = [2, 5, 8, 0]
-    
+    #처음 시작할 때 손가락의 위치
     left_finger_position = [3, 0]
     right_finger_position = [3, 2]
     
@@ -87,11 +90,10 @@ def solution_a(numbers, hand):
         elif number in right_finger_numbers:
             result += 'R'
             right_finger_position = keypad[number]
-            
         else:
+            #오른쪽 왼쪽의 거리를 측정
             left_finger_distance = get_distance(keypad, left_finger_position, number)
             right_finger_distance = get_distance(keypad, right_finger_position, number)
-            
             if left_finger_distance > right_finger_distance:
                 result += 'R'
                 right_finger_position = keypad[number]
@@ -99,11 +101,10 @@ def solution_a(numbers, hand):
                 result += 'L'
                 left_finger_position = keypad[number]
             elif left_finger_distance == right_finger_distance:
+                #upper 좋은 아이디어
                 result += hand[0].upper()
                 if hand == 'right':
                     right_finger_position = keypad[number]
                 elif hand == 'left':
                     left_finger_position = keypad[number]
-    
     return result
-
